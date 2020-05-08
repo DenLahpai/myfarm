b9d7c7e32d25f2827ae099a53bc1cc06a108326a
DenLahpai <den.lahpai@icloud.com>
2020-05-08 16:10:02 +0630

    fixed head

--- EN/includes/footer.php
+++ EN/includes/footer.php
@@ -1,10 +1,8 @@
 
     <div class="footer-links">
-        <a href="#">About Us</a> |
+        <a href="aboutus.html">About Us</a> |
         <a href="contactus.html">Contact Us</a> |
         <a href="#">Career</a> |
-        <a href="#">About Us</a> |
-        <a href="#">Contact Us</a> |
         <a href="#">Career</a> |
     </div>
     <div class="footer-social-net">
