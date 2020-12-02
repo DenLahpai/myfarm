27e86d1ff6c505433ea74c7bdf9fd0c818a75e8c
DenLahpai <den.lahpai@icloud.com>
2020-12-02 09:51:15 +0630

    fixed my_bookmarks.php

--- EN/includes/my_bookmarks.php
+++ EN/includes/my_bookmarks.php
@@ -136,7 +136,7 @@ $rows_Posts = table_Posts ($job, $row_Users->Link, NULL, $sorting, $limit);
 		<div class="post-item">
 			<div class="comment-input-items">
 				<div class="comment-input-item">
-					<span class="textarea" id="<? echo "Comment".$row_Posts->PostsId; ?>" role="textbox" contenteditable onclick="checkComment('<? echo $row_Posts->PostsId; ?>')"></span>
+					<span class="textarea" id="<? echo "Comment".$row_Posts->PostsId; ?>" role="textbox" contenteditable onclick="checkComment('<? echo $row_Posts->PostsId; ?>')">Write your comments...</span>
 				</div>
 				<div class="comment-input-item">
 					<button type="button" class="btn-comment" id="<? echo "btn-comment".$row_Posts->PostsId;?>" onclick="postComment('<? echo $row_Posts->PostsId;?>', 'select_posts.php');">Post Comment!</button>
@@ -236,6 +236,28 @@ $rows_Posts = table_Posts ($job, $row_Users->Link, NULL, $sorting, $limit);
 											<div style="font-style: italic; font-size: 0.6em; color: var(--leaf-color); text-align: right;">
 												<?php echo date("d-M-y H:i", strtotime($row_Replies->Created)); ?>
 											</div>
+											<!-- starts here -->
+											<div class="comments-item">
+												<div class="comment-commands">
+													<div onclick="Toggle('<? echo "re-reply-input".$row_Replies->Id; ?>');">
+														Reply
+													</div>
+													<div>
+														Report <span class="symbols">&#10071;</span>
+													</div>															
+												</div>
+												<div class="reply" id="<? echo 're-reply-input'.$row_Replies->Id; ?>">
+													<div>
+														<span class="textarea" id="<? echo 'Re-Reply'.$row_Replies->Id; ?>" role="textbox" contenteditable onclick=""><? echo "@".$row_Users->Username." | "; ?>
+														</span>
+													</div>
+													<div>
+														<!-- <button onclick="alert('<? echo $row_Replies->Id; ?>');">Test</button> -->
+														<button class="btn-comment" style="display: block;" onclick="insertReReply('<? echo $row_Replies->Id; ?>', '<? echo $row_Comments->Id ;?>');">Post Reply!</button>
+													</div>
+												</div>														
+											</div>
+											<!-- ends here -->
 										</div>							
 									</div>
 								<?php endforeach ?>
