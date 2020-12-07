2dba5979ae47961119580f3a2a7f0f3572e764c7
DenLahpai <den.lahpai@icloud.com>
2020-12-07 12:59:18 +0630

    finished contents of footer.php MM in Unicode Myanmar

--- MM/includes/footer.php
+++ MM/includes/footer.php
@@ -1,10 +1,10 @@
     <div class="footer-links">
-        <a href="aboutus.html">About Us</a> |
-        <a href="contactus.html">Contact Us</a> |
-        <a href="#">Career</a> |        
+        <a href="aboutus.html">ကျွနု်ပ် တို့ အကြောင်း</a> |
+        <a href="contactus.html">ဆက်သွယ်ရန်</a> |
+        <a href="#">အလုပ်အကိုင်များ</a> |        
     </div>
     <div class="footer-social-net">
-        Find us on: <br>
+        ဆက်သွယ်ရန်: <br>
         <img src="../images/facebook-icon.png" alt="">
     </div>
     <div class="legal">
